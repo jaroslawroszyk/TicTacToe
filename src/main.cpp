66b078478022a43e09df0a25fc0794e6ce8e981c
jaroslawroszyk <roszyk.jarek@gmail.com>
2021-08-08 15:11:55 +0200

    FIX

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,10 @@
-#include "Board.hpp"
+#include "Board.h"
 #include "TictactoeMode.h"
 #include <iostream>
 int main()
 {
     Board board;
-    
+    menu();
     board.drawBoard();
     auto [row, col, mark]  = board.getUserInput();
     board.refresh(row, col, mark);
