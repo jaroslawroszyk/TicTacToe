5f685cf66d401637c489f902e3b54c3d9ffa665d
iprice73 <mateusz.kowalski074@gmail.com>
2021-08-08 11:28:17 +0200

    drawing board and simple user input

--- src/Board.cpp
+++ src/Board.cpp
@@ -3,6 +3,34 @@
 #include <iostream>
 #include <string>
 
-Board::Board() {
-    std::cout << "test\n";
+void Board::drawBoard() const {
+    std::cout << "----1---2---3--\n";
+    size_t row_cnt{1};
+    for (const auto& row : m_board) {
+        std::cout << row_cnt++ << " | ";
+        for (const auto field : row) {
+            std::cout << field << " | ";
+        }
+        std::cout << '\n';
+    }
+    std::cout << "---------------\n";
+}
+
+void Board::refresh(size_t row, size_t col, char mark) {
+    mark = static_cast<char>(std::toupper(mark));
+    if (m_board[row - 1][col - 1] == '_' and (mark == 'X' or mark == 'O')) {
+        m_board[row - 1][col - 1] = mark; // -1 cus of array indexing
+    }
+}
+
+Input Board::getUserInput() {
+    size_t row, col;
+    char mark;
+    std::cout << "Row: ";
+    std::cin >> row;
+    std::cout << "Col: ";
+    std::cin >> col;
+    std::cout << "Mark: ";
+    std::cin >> mark;
+    return {row, col, mark};
 }
