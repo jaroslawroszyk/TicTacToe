21a87a56ff78a2d4e2514552cc19afac109f6ad1
jaroslawroszyk <roszyk.jarek@gmail.com>
2021-08-08 23:11:41 +0200

    INIT

---
+++ inc/Game.h
@@ -0,0 +1,37 @@
+#pragma once
+#include "Board.h"
+#include <iostream>
+#include "Easy.h"
+
+enum class MenuMode
+{
+    player_vs_player = 1,
+    player_vs_EasyAi = 2,
+    player_vs_MediumAi = 3,
+    player_vs_HardAi = 4,
+    exit = 5,
+};
+
+
+
+class Game
+{
+public:
+    void playerVsPlayer();
+    void playerVsEasyAi();
+    void menu();
+    //todo getscore count score
+    int getScore() const
+    {
+        return score;
+    }
+private:
+    Board board;
+    int score{};
+    bool round{};
+};
+
+std::istream &operator>>(std::istream &in, MenuMode &enter);
+
+
+
